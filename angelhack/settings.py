ce88eeb51c668a0a17495070e4c9a59f54689ad9
Shrenik <shrenik.jain@research.iiit.ac.in>
2016-05-28 15:11:21 +0530

    App configured for UI

--- angelhack/settings.py
+++ angelhack/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'selector'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'angelhack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- angelhack/urls.py
+++ angelhack/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from selector.views import startpage
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',startpage)
 ]
